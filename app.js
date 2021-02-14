de37cf221d7f5c46d45c9522eead70e7a1d17e00
IngEoGeoBuk <>
2021-02-14 22:08:41 +0900

    net-ninja

---
+++ app.js
@@ -0,0 +1,40 @@
+// server.js에 있는 코드보다는 app.js 코드 식으로 하는게 훨씬 좋다.
+const express = require('express');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+const blogRoutes = require('./routes/blogRoutes');
+
+// express app
+const app = express();
+
+// connect to mongodb
+const dbURI = 'mongodb+srv://cobain:qkdgkahr7365@cobain.j71f9.mongodb.net/note-tuts?retryWrites=true&w=majority';
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+.then((result) => app.listen(3000))
+.catch((err) => console.log(err));
+
+// register view engine
+app.set('view engine', 'ejs');
+
+// middleware & static files
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan('dev'));
+
+//routes
+app.get('/', (req, res) => {
+    res.redirect('/blogs');
+});
+
+app.get('/about', (req, res) => {
+    // res.send('<p>about page</p>');
+    res.render('about', { title: 'About' });
+});
+
+// blog routes
+app.use('/blogs', blogRoutes);
+
+// 404 page
+app.use((req, res) => {
+    res.status(404).render('404', { title: '404' });
+});
